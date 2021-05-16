c442ebf185e45f9bcc77a78d92ded13985df9b1a
Stefan Horst <stefan@ultrachaos.de>
2021-05-16 21:22:26 +0200

    fix shasum

--- metadata.py
+++ metadata.py
@@ -6,7 +6,7 @@ if node.has_bundle('check_mk_agent'):
             'mk_apt': {
                 'type': 'check_mk_plugin',
                 'run_every': 3600,
-                'sha256': "d9d9865087b1ae20ba4bd45446db84a96d378c555af687d934886219f31fecb0",
+                'sha256': "de14f88bf8b6668151086ac4a8e156d5eea2315a159d208136dfee24838d2e2c",
             }
         }
     }
